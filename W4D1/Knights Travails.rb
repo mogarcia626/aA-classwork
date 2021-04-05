0af273b787354d630f77b6136636820995a94593
moustafa <mogarcia626@gmail.com>
2021-04-05 13:58:25 -0400

    Started Pathfinder

---
+++ W4D1/Knights Travails.rb
@@ -0,0 +1,28 @@
+class KnightPathFinder
+    attr_accessor :parent, :children
+
+    def self.valid_moves(position) #[x,y]
+                [6,6]
+        moves = [ [1,2], [-1,-2]  ....  ]
+
+
+    end
+
+    def initialize(position)
+        @root_node = position
+        @parent = @root_node
+        @children = []
+    end
+
+    def new_move_positions
+
+    end
+
+
+
+
+
+
+    
+
+end
