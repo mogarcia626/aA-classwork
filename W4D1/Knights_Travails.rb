5539aaac8b2e4afb986fec1c438f91074896e6e5
moustafa <mogarcia626@gmail.com>
2021-04-05 20:15:47 -0400

    Completed Knights Travail

--- W4D1/Knights_Travails.rb
+++ W4D1/Knights_Travails.rb
@@ -1,24 +1,30 @@
+require_relative "00_tree_node"
+
 class KnightPathFinder
     attr_accessor :parent, :children
+    attr_reader :root_node, :considered_positions    
 
     def self.valid_moves(pos) #[x,y]
         moves = [ [1,2], [-1,2], [-1,-2], [1,-2], [2,1], [-2,1], [-2,-1], [2,-1] ]
         valid_moves = []
         moves.each do |mov|
-            if pos[0] + mov[0] < 8 && pos[1] + mov[1] < 8
-                valid_moves << mov
+            if pos[0]+mov[0]<8 && pos[1]+mov[1]<8 && pos[0]+mov[0]>=0 && pos[1]+mov[1] >= 0
+                valid_moves << [ pos[0]+mov[0] , pos[1]+mov[1] ]
             end
         end
         valid_moves        
     end
 
+
+
     def initialize(start_position)        
         @start_position = start_position
         @root_node = PolyTreeNode.new(start_position)        
-        @considered_positions = []
+        @considered_positions = [start_position]
+        build_move_tree
     end
 
-    def new_move_positions(parent_node)
+    def new_move_positions(parent_node) #take in position
         moves = KnightPathFinder.valid_moves(parent_node.value) 
         moves.reject! { |move| @considered_positions.include?(move) }   #remove dup moves
         moves.each { |move| @considered_positions << move }     #adds to 'considered pos' arr
@@ -26,76 +32,52 @@ class KnightPathFinder
         nodes_arr.each do |node|    #sets the parent and child node relationship
             node.parent = parent_node
         end
+        # p nodes_arr
         return nodes_arr #returns arr w/ nodes with parent children relationships set
     end
 
     def build_move_tree
-        remaining_pos = [@start_position]
+        remaining_pos = [@root_node]
 
         while remaining_pos.length > 0
             remaining_pos.each do |pos| # SP
-                remaining_pos += next_move_positions(pos)
+                remaining_pos += new_move_positions(pos)
                 remaining_pos.shift
             end
         end
     end
+
     # search for end_pos in the move tree
     # return the tree node instance containing end_pos
     # returns a node
 
     # thoughts: call BMT, find path,
     def find_path(end_pos)
-        build_move_tree
+        
         nodes_arr = [root_node]
-        
-        
 
-    end
-
-    # race back from the node to the root using PolyTreeNode#parent
-    # returns array of positions
-    def trace_path_back 
-
-    end
-end
-
-=begin
-    def parent=(other_node)
-        unless other_node == nil
-            @parent.children.reject! { |ele| ele == self } if @parent != nil
-            @parent = other_node
-                if !other_node.children.include?(self) 
-                    other_node.children << self
-                end
-        else
-            @parent = nil
-        end 
-    end
-
-    def bfs(target_value)
-        arr = [self]
-        
-        while arr.length > 0            
-            arr.each do |node|
-                return node if node.value == target_value                
-                arr += node.children 
-                arr.shift                
+        while nodes_arr.length > 0
+            nodes_arr.each do |node|
+                return trace_path_back(node) if node.value == end_pos
+                nodes_arr += node.children
+                nodes_arr.shift
             end
         end
-        nil
+        nil     
     end
 
-    def initialize(position)
-        @position = position
-        @parents ||= []
-        @parents += position # parents = Set of current & previous positions
-        @children = [] # possible future moves 
+#     # race back from the node to the root using PolyTreeNode#parent
+#     # returns array of positions
+    def trace_path_back(end_node)
+        return [end_node.value] if !end_node.parent
+        trace_path_back(end_node.parent) + [end_node.value] 
     end
 
+end
+
+k = KnightPathFinder.new([0,0])
+
+p k.find_path([7,6])
+p k.find_path([6,2])
 
 
-    def self.on_board?(pos)
-        pos.all? {|i| i < 8}
-    end
-    
-=end
