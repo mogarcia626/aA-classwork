d8bbbea617898fe88b988d523cbed507c030b7f0
“dennis” <dennisyu225@gmail.com>
2021-04-08 17:11:50 -0700

    Completed specs for Towers of Hanoi

--- W4D4/lib/first_TDD_projects.rb
+++ W4D4/lib/first_TDD_projects.rb
@@ -55,25 +55,79 @@ end
 # 3 sub-arrays  
 
 class Game   #create an array length # of stacks # of pieces 
-    def initialize(stacks, pieces)
+
+    def initialize(num_stacks, num_pieces)
         #@stacks  2D array of stacks
         #@num of pieces
+        # set_up_game 
     end
 
+    def set_up_game 
+        # create stacks and populate first stack with pieces 
+        # 
+    end
     
+    def get_move 
+        # gets move and verifies 
+        # take_piece
+        # drop_piece 
+        # return a position or an error prompt user for another position request 
+
+    end
+
+    def make_move 
+        # execute move 
+    end
+
+    def inspect 
+        @index 
+    end
+
+    def won?
+
+    end
     
 end
 
+class Column
+
+    attr_reader :index 
+
+    def initialize(index)
+        @stack = [] 
+        @index = index 
+    end
+
+    def take_piece 
+        # pull from top 
+        # ensure stack is not empty 
+        # self[i] = new_move 
+        # should return the piece that was taken 
+    end
+
+    def drop_piece(piece)
+        # add to top
+        # ensure value is a piece is lower than piece inside of stack 
+    end
+
+end
+
+
 
 class Piece
+
     def initialize(value)
         @value = value
     end
 
+    def inspect 
+        @value 
+    end
+
 end
 
 
 
-stack   stack   stack   
+# stack   stack   stack   
 
 
--- W4D4/spec/first_TDD_projects_spec.rb
+++ W4D4/spec/first_TDD_projects_spec.rb
@@ -54,4 +54,79 @@ describe '#stock_picker' do
 
 end
 
-describe "#"
+
+
+
+
+
+
+
+describe Piece do 
+
+    describe '#initialize' do 
+        it 'should equal value' do 
+            expect(@value).to eq value 
+        end
+    end
+
+end
+
+describe Column do 
+    subject(:column) { Column.new(3) } 
+
+    describe '#initialize' do 
+        it 'should have @stack equal empty array' do 
+            expect(@stack).to eq ([])
+        end
+        it 'should take the index' do 
+            expect(@index).to eq index 
+        end
+    end
+
+    describe '#take_piece' do 
+
+
+        it 'ensure stack is empty' do 
+            expect(column.take_piece).to raise_error ('this stack is empty')
+        end
+        it 'should pull from top and return the piece that was taken' do 
+            (1..3).each { |i| column.stack << Piece.new(i) }
+            expect(column.take_piece).to eq (3)
+            expect(column.stacks[-1].index).to eq (2)
+        end
+
+
+    end
+
+    describe '#drop_piece' do
+
+        it 'raise an error if value of piece in hand is greater than piece in stack' do 
+            column.stack << Piece.new(1)
+            piece = Piece.new(2)
+            expect(column.drop_piece(piece)).to raise_error 'invalid move, piece too large'
+        end
+        it 'add pieces to the top of stack and returns true when completed' do 
+            column.stack << Piece.new(1)
+            piece1 = Piece.new(0)
+            expect(column.drop_piece(piece1)).to eq true 
+            expect(column.stacks[-1].value).to eq (0)
+        end
+
+
+    end
+
+
+end
+
+
+
+        # it 'ensure stack is empty' do 
+        #     expect(column.empty?).to raise_error ('this stack is empty')
+        # end
+        # it 'should pull from top' do 
+        #     expect(column.take_piece.index[-1].value).to eq (2)
+        # end
+        # it 'should return the piece that was taken' do 
+        #     expect(column.take_piece).to eq (3)
+        # end
+
