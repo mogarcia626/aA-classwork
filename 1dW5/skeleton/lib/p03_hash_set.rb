ad3d9c652dd167553baa6320c6a22aeecd3c9720
Josiah Leon <sjosiahleon@gmail.com>
2021-04-12 14:58:25 -0700

    Started hash_set.rb

--- 1dW5/skeleton/lib/p03_hash_set.rb
+++ 1dW5/skeleton/lib/p03_hash_set.rb
@@ -4,21 +4,29 @@ class HashSet
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(key)
+    self[key.hash] << key
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      # @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @num_buckets]
   end
 
   def num_buckets
