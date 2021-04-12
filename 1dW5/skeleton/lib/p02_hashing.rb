254e0c329787dcf606eaffdeccc9b16eae7174bd
Josiah Leon <sjosiahleon@gmail.com>
2021-04-12 14:48:39 -0700

    Created Hashing.rb

--- 1dW5/skeleton/lib/p02_hashing.rb
+++ 1dW5/skeleton/lib/p02_hashing.rb
@@ -6,6 +6,7 @@ class Array
   def hash
     answer = ""
     self.each_with_index do |ele, idx|
+      ele = ele.to_i if ele.is_a?(String)
       answer += (ele*5).to_s if idx.even?
       answer += (idx**2).to_s if ele.even?
       answer += (ele**2).to_s if idx.odd?
@@ -16,10 +17,9 @@ class Array
  
 end
 
-p [1,2,3].hash
-
 class String
   def hash
+    self.bytes.hash
   end
 end
 
@@ -27,6 +27,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = self.keys.map(&:to_s) + self.values.map(&:to_s)
+    arr.hash
   end
 end
