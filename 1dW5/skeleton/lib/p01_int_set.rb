310371a7240c59bf247294e4ed5b96a3378596bd
Josiah Leon <sjosiahleon@gmail.com>
2021-04-12 14:01:49 -0700

    Created IntSet

--- 1dW5/skeleton/lib/p01_int_set.rb
+++ 1dW5/skeleton/lib/p01_int_set.rb
@@ -1,14 +1,15 @@
 class MaxIntSet
+
+  attr_reader :store
+  
   def initialize(max)
     @max = max
     @store = Array.new(max,false) 
   end
 
   def insert(num)   
-    raise "Out of bounds" unless (0..@max).include?(num)
-    @store[num] = true
-    # p @store    
-    return true
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true   
   end
 
   def remove(num)
@@ -16,11 +17,13 @@ class MaxIntSet
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num <= @max && num >= 0
   end
 
   def validate!(num)
@@ -31,21 +34,26 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @num_buckets]
   end
 
   def num_buckets
@@ -58,22 +66,29 @@ class ResizingIntSet
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
     @count = 0
   end
 
   def insert(num)
+    unless self[num].include?(num)
+      self[num] << num 
+      @count += 1
+    end
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
-  private
 
+  private
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @num_buckets]
   end
 
   def num_buckets
