8b609747e8181c3bd0ab029df6d1ee45994d6f36
moustafa <mogarcia626@gmail.com>
2021-04-22 19:44:43 -0400

    Created artwork class

---
+++ 3dW6/first_routes_and_controllers/app/models/artwork.rb
@@ -0,0 +1,9 @@
+class Artwork < ApplicationRecord
+
+    validates :artist_id, uniqueness: {scope: :title} 
+
+    belongs_to :artist,
+    foreign_key: :artist_id,
+    class_name: :User
+    
+end
