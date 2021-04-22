99c7c1597932474a691075b3075a6719e9978d4c
moustafa <mogarcia626@gmail.com>
2021-04-22 18:09:11 -0400

    Finished users_controller

--- 3dW6/first_routes_and_controllers/app/controllers/users_controller.rb
+++ 3dW6/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -6,17 +6,37 @@ class UsersController < ApplicationController
   end
 
   def create
-    render json: params
-    users = User.new(params.require(:user).permit(:name, :email))
-    users.save!
-    render json: users
+    # render json: params
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    render json: User.find_by(id: params[:id])
   end
 
-  
+  def update
+    user = User.find_by(id: params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
 
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 
 end
