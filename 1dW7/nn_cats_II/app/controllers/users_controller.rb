226e4bd278290da13f3ffe267fcae6742acbf6c0
moustafa <mogarcia626@gmail.com>
2021-04-26 17:29:17 -0400

    Created Controllers

---
+++ 1dW7/nn_cats_II/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            # login(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+end
--- 1dW7/nn_cats_II/app/models/user.rb
+++ 1dW7/nn_cats_II/app/models/user.rb
@@ -1,7 +1,29 @@
 class User < ApplicationRecord
+
     def reset_session_token!
         self.session_token = SecureRandom::urlsafe_base64
         self.save!
         self.session_token
     end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password        
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(username: user_name)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    
 end
