81edfa58f4a10e288699839f37da74226c2a1e96
ziosanfer <ziosanfer@gmail.com>
2021-04-26 13:56:00 -0700

    created user migration and started model

---
+++ 1dW7/nn_cats_II/db/migrate/20210426190953_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, uniqueness: true
+      t.string :password_digest, length: {minimum: 6, nil: true}
+      t.string :session_token, uniqueness: true, null: false
+      t.timestamps
+    end
+    add_index :users, :session_token
+  end
+end
