955a708986c284647214240b07216ba0eb42fffa
moustafa <mogarcia626@gmail.com>
2021-04-26 20:25:46 -0400

    Wrapped up

--- 1dW7/nn_cats_II/app/controllers/application_controller.rb
+++ 1dW7/nn_cats_II/app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     def curr_user
         @curr_user ||= User.find_by(session_token: session[:session_token])
     end
+
+    def log_out
+        @curr_user.reset_session_token! if !!curr_user            
+        session[:session_token] = nil       
+    end
+    
 end
