bd051898fcf3484e69fed4c59afc8c797fefe4bb
moustafa <mogarcia626@gmail.com>
2021-05-03 18:42:27 -0400

    Finished Post and Sub controllers

---
+++ 1dW8/reddit_app/app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :require_login, except: [:show]
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    render :show
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    if @post.update(post_params)
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors]=@post.errors.full_messages
+      render :edit
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors]=@post.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find_by(id: params[:id])
+    @post.destroy if @post
+    redirect_to sub_url(@post.sub_id)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :content, :sub_id)
+  end
+end
+
--- 1dW8/reddit_app/app/controllers/subs_controller.rb
+++ 1dW8/reddit_app/app/controllers/subs_controller.rb
@@ -1,25 +1,50 @@
 class SubsController < ApplicationController
+  before_action :require_login, only: [:new, :create, :edit, :update]
+
   def index
     @subs = Sub.all
-
     render :index
   end
 
   def show
-    @sub = Sub.find_by(id: )
+    @sub = Sub.find_by(id: params[:id])
+    redirect_to sub_url(@sub)
   end
 
   def new
+    @sub = Sub.new
+    render :new
   end
 
   def create
+    @sub = Sub.new(sub_params)
+    @sub.moderator_id = current_user.id
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors]=@sub.errors.full_messages
+      render :new
+    end
   end
 
 
   def edit
+    @sub = Sub.find_by(id: params[:id])
+    render :edit
   end
 
   def update
+    @sub = Sub.find_by(id: params[:id])
+    if @sub.moderator_id==current_user.id && @sub.update(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors]=@sub.errors.full_messages
+      render :edit
+    end
   end
 
+  private
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
 end
