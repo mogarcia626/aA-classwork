abd383531813b2f92fc047f3427e35442bdc751c
moustafa <mogarcia626@gmail.com>
2021-05-20 19:00:18 -0400

    More Todos

--- to_dos/frontend/store/store.js
+++ to_dos/frontend/store/store.js
@@ -1,5 +1,7 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer'
+import { receiveTodos } from '../actions/todo_actions';
+
 
 const configureStore = (preloadedState = {}) => {
     const store = createStore(rootReducer, preloadedState);
