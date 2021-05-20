a79261f4d044db4ef31f67037eada401f9774a0b
moustafa <mogarcia626@gmail.com>
2021-05-20 18:27:00 -0400

    ToDos

---
+++ to_dos/frontend/actions/todo_actions.js
@@ -0,0 +1,19 @@
+export const RECEIVE_TODO = 'RECEIVE_TODO';
+export const RECEIVE_TODOS = 'RECEIVE_TODOS';
+// export const REMOVE_TODO = 'REMOVE_TODO';
+// export const TODO_ERROR = 'TODO_ERROR';
+
+
+export const receiveTodo = (todo) => {
+    return {
+        type: RECEIVE_TODO,
+        todo,
+    };
+};
+
+export const receiveTodos = todos => ( {
+    type: RECEIVE_TODOS,
+    todos    
+});
+
+
---
+++ to_dos/frontend/todo_redux.jsx
@@ -0,0 +1,21 @@
+import configureStore from './store/store';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('in event listener')
+    const store = configureStore();
+    window.store = store;
+    window.store.getState(); 
+    const root = document.getElementById('root');
+});
+
+
+// const newTodos = [{ id: 1, ...etc }, { id: 2, ...etc }, ...etc];
+// store.getState(); // should return default state object
+// store.dispatch(receiveTodo({ id: 3, title: "New Todo" }));
+// store.getState(); // should include the newly added todo
+// store.dispatch(receiveTodos(newTodos));
+// store.getState(); // should return only the new todos
+
+// const newTodos = [{id: 1, title:'homework'}, {id:2, title:'reading'}, {id:3, title:'exercise'}];
