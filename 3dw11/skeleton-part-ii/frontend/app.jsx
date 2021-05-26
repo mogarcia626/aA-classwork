3df059372d999aaffab83af97cff9c8db969b414
moustafa <mogarcia626@gmail.com>
2021-05-26 16:23:41 -0400

    Started pokedexii w11d3

---
+++ 3dw11/skeleton-part-ii/frontend/app.jsx
@@ -0,0 +1,6 @@
+import { Route } from "react-router-dom";
+import PokemonIndexContainer from './components/pokemon/pokemon_index_container';
+
+const App = () => (
+    <Route path="/" component={PokemonIndexContainer} />
+)
