ba0d2541413ad0bfb0e0839065dec78806b14822
moustafa <mogarcia626@gmail.com>
2021-05-27 17:10:48 -0400

    Bench

--- 4dw11/bench_bnb/config/routes.rb
+++ 4dw11/bench_bnb/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    root to: 'static_pages#root'
